2a06365591511f6a87a14dea30bab1b3e655dec4
m-kostrzewa <contact@michal-kostrzewa.com>
2026-03-01 18:16:49 +0100

    Add infinite scroll.

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 
 import { liteClient as algoliasearch } from 'algoliasearch/lite';
-import { InstantSearch, InfiniteHits, Configure, useInstantSearch } from 'react-instantsearch';
+import { InstantSearch, useInfiniteHits, Configure, useInstantSearch } from 'react-instantsearch';
 
 import Firebase, { FirebaseContext } from './components/firebase';
 
@@ -24,6 +24,37 @@ function NoResultsBoundary({ children }) {
   return children;
 }
 
+function AutoInfiniteHits() {
+  const { hits, showMore, isLastPage } = useInfiniteHits();
+  const sentinelRef = useRef(null);
+
+  useEffect(() => {
+    if (!sentinelRef.current) return;
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting && !isLastPage) {
+          showMore();
+        }
+      });
+    });
+    observer.observe(sentinelRef.current);
+    return () => observer.disconnect();
+  }, [showMore, isLastPage]);
+
+  return (
+    <div className="ais-InfiniteHits">
+      <ul className="ais-InfiniteHits-list">
+        {hits.map((hit) => (
+          <li key={hit.objectID} className="ais-InfiniteHits-item">
+            <QuotationComponent hit={hit} />
+          </li>
+        ))}
+      </ul>
+      <div ref={sentinelRef} style={{ height: '1px' }} />
+    </div>
+  );
+}
+
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -40,7 +71,7 @@ root.render(
             <Configure hitsPerPage={10} />
             <CogitatorComponent />
             <NoResultsBoundary>
-              <InfiniteHits hitComponent={QuotationComponent} />
+              <AutoInfiniteHits />
             </NoResultsBoundary>
           </InstantSearch>
         </article>
